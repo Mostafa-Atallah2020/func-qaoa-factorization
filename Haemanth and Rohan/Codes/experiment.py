f375af4b5df002aff3fc74ae3ca3ad16713e0a3c
Ludmila Botelho <ludmilaasb@gmail.com>
2022-12-14 16:27:18 +0100

    made paralelized version

--- Haemanth and Rohan/Codes/experiment.py
+++ Haemanth and Rohan/Codes/experiment.py
@@ -1,14 +1,17 @@
 from utils import construct_and_get_info_of_QAOA_modulo_layer_circuit, get_gates
 import csv
 import numpy as np
+# from joblib import Parallel, delayed
+
 
 if __name__ == "__main__":
 
-
+    from joblib import Parallel, delayed
+    import pickle
     import argparse
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("-p", "--maxpow", type=int, default=10, required=False)
+    parser.add_argument("-p", "--maxpow", type=int, default=20, required=False)
     parser.add_argument("-n", "--number", type=int, default=100, required=False)
     parser.add_argument("-l", "--levels", type=int, default=1,required=False)
     parser.add_argument("--dirin", type=str, default="final_data_biprimes")
@@ -21,7 +24,7 @@ if __name__ == "__main__":
     number_of_biprimes = args.number
     max_pow = args.maxpow
     lvl = args.levels
-    filename = f"../../Mostafa and Ludmilla/{args.dirin}/biprimes_maxpow_{max_pow}_number_{number_of_biprimes}.csv"
+    filename = f"../../Mostafa and Shamim/{args.dirin}/biprimes_maxpow{max_pow}_number{number_of_biprimes}.csv"
     # filename = f"./{args.dirin}/biprimes_maxpow{max_pow}_number{number_of_biprimes}.csv"
     
     biprimes = []
@@ -33,9 +36,16 @@ if __name__ == "__main__":
             int_row = [int(i) for i in row]
             biprimes.append(int_row)
 
-    data = np.asarray(biprimes)[:,-1]
 
-    num_qubits, num_clbits, depths, num_gates, n = construct_and_get_info_of_QAOA_modulo_layer_circuit(data, lvl = lvl)
-    cx_gates, u3_gates = get_gates(num_gates)
+    num_chunks = 10
+    data = np.asarray(biprimes)[:,-1]
+    results = Parallel(n_jobs=8)\
+    (delayed(construct_and_get_info_of_QAOA_modulo_layer_circuit)([number],lvl=lvl) for number in data)
+
+    with open(f"results/data_maxpow{max_pow}_number{number_of_biprimes}.pkl", "wb") as file:
+        pickle.dump(results, file)
+
+    # results = construct_and_get_info_of_QAOA_modulo_layer_circuit(data, lvl = lvl)
+    # cx_gates, u3_gates = get_gates(num_gates)
 
     
