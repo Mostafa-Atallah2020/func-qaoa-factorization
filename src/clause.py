c7f16792f0e660332d1012407f997fcdb7f66b6b
Mostafa Atallah <m.ataallah.2014@gmail.com>
2023-06-05 10:23:53 +0300

    Alternative implementation for space reduction algorithm

--- src/clause.py
+++ src/clause.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 from IPython.display import display
 from sympy import simplify, Add
+import itertools
 
 
 class BitsTable:
@@ -32,6 +33,26 @@ class Clause:
         self.bits = self.__clause.free_symbols
         self.pq_part, self.z_part = self.__split()
 
+    def __table_from_expr(self, expr):
+        # Create a list of variables
+        variables = expr.free_symbols
+        # Generate all possible combinations of 0 and 1 for the variables
+        combinations = list(itertools.product([0, 1], repeat=len(variables)))
+
+        table = []
+        for comb in combinations:
+            # Substitute variables in z_expr and evaluate the expression
+            expr_val = expr.subs(zip(variables, comb)).evalf()
+
+            # Append the combination and corresponding value to the table
+            table.append(list(comb) + [expr_val])
+
+        # Create pandas DataFrames for each table
+        df = pd.DataFrame(
+            table, columns=[str(variable) for variable in variables] + ["value"]
+        )
+        return df
+
     def __split(self):
         coeff_dict = self.__clause.as_coefficients_dict()
         z_expr = Add(
@@ -41,13 +62,26 @@ class Clause:
         return other_expr, z_expr
 
     def reduce_space(self):
-        variables = list(self.bits)
-        values = product(range(2), repeat=len(variables))
-        reduced_space = []
-        for v in values:
-            subs_dict = dict(zip(variables, v))
-            subs_expr = self.__clause.subs(subs_dict)
-            if simplify(subs_expr) == 0:
-                reduced_space.append(v)
-        df = pd.DataFrame(reduced_space, columns=variables).astype(np.uint8)
-        return BitsTable(df)
+        combined_table = []
+        z_part, pq_part = self.__split()
+        z_table = self.__table_from_expr(z_part)
+        pq_table = self.__table_from_expr(pq_part)
+
+        for z_row in z_table.iterrows():
+            z_row_values = z_row[1].values[:-1]  # Exclude the last column ('value')
+            z_row_sum = z_row[1]["value"]
+
+            for pq_row in pq_table.iterrows():
+                pq_row_values = pq_row[1].values[:-1]
+                pq_row_sum = pq_row[1]["value"]
+
+                if z_row_sum + pq_row_sum == 0:
+                    combined_row = list(z_row_values) + list(pq_row_values)
+                    combined_table.append(combined_row)
+
+        # Create pandas DataFrame for the combined table
+        variables_z = list(z_table.columns)[:-1]
+        variables_pq = list(pq_table.columns)[:-1]
+        combined_columns = variables_z + variables_pq
+        combined_df = pd.DataFrame(combined_table, columns=combined_columns)
+        return BitsTable(combined_df)
