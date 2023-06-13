3e2e0809d33f17d21567f96be51b19d110a70b5c
Mostafa Atallah <m.ataallah.2014@gmail.com>
2023-06-13 13:54:00 +0300

    Added: comments and docstrings to SetsGraph

--- src/sets_graph.py
+++ src/sets_graph.py
@@ -5,6 +5,12 @@ import itertools
 
 class SetsGraph:
     def __init__(self, sets) -> None:
+        """
+        Initializes a SetsGraph object.
+
+        Args:
+            sets (list): A list of sets to be represented as nodes in the graph.
+        """
         self.__sets = sets
         self.__graph = nx.Graph()
         self.__add_nodes()
@@ -14,6 +20,12 @@ class SetsGraph:
         self.__get_disjoint_sets()
 
     def __get_disjoint_sets(self):
+        """
+        Finds the maximum independent set of nodes in the graph.
+
+        Returns:
+            list: A list of sets representing the maximum independent set of nodes.
+        """
         # Generate all possible combinations of nodes
         all_nodes = set(range(len(self.__sets)))
         independent_sets = []
@@ -37,6 +49,9 @@ class SetsGraph:
         [self.disjoint_sets.append(self.__sets[node]) for node in max_independent_set]
 
     def draw(self):
+        """
+        Draws the graph with labeled nodes.
+        """
         # Draw the graph with labeled nodes
         pos = nx.spring_layout(self.__graph)
         nx.draw(
@@ -56,11 +71,17 @@ class SetsGraph:
         plt.show()
 
     def __add_nodes(self):
+        """
+        Adds nodes to the graph and labels them with their corresponding set.
+        """
         # Add nodes to the graph and label them with their corresponding set
         for i, s in enumerate(self.__sets):
             self.__graph.add_node(i, set=s)
 
     def __add_edges(self):
+        """
+        Adds edges to the graph between nodes that have intersecting sets.
+        """
         for i in range(len(self.__sets)):
             for j in range(i + 1, len(self.__sets)):
                 if self.__sets[i].intersection(self.__sets[j]):
