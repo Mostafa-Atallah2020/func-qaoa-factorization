f4fea3fdcc9f9b7337a40d84cb182f2d56213982
Mostafa Atallah <m.ataallah.2014@gmail.com>
2023-06-21 19:01:00 +0300

    Define some helper functions

--- src/clause_utils.py
+++ src/clause_utils.py
@@ -1,5 +1,6 @@
 import numpy as np
 from tabulate import tabulate
+import pandas as pd
 from sympy import (
     Add,
     Mul,
@@ -7,6 +8,146 @@ from sympy import (
     Symbol,
     sympify,
 )
+
+
+def convert_to_dataframe(data, columns=["Key", "Value"]):
+    """
+    Convert a dictionary to a DataFrame.
+
+    Args:
+    -----
+
+        `data` (`dict`): The dictionary to be converted.
+
+    Returns:
+    --------
+
+        `pandas.DataFrame`: The resulting DataFrame.
+
+    """
+    df = pd.DataFrame(data.items(), columns=columns)
+    return df
+
+
+def merge_dictionaries(*dicts):
+    """
+    Merge multiple dictionaries into a single dictionary.
+
+    Args:
+    -----
+
+        `*dicts`: Multiple dictionaries to be merged.
+
+    Returns:
+    --------
+
+        `dict`: Merged dictionary containing all the key-value pairs from the input dictionaries.
+
+    """
+    merged_dict = {}
+
+    for dictionary in dicts:
+        merged_dict.update(dictionary)
+
+    return merged_dict
+
+
+def find_non_matching_values(dictionary):
+    """
+    Find key-value pairs in a dictionary where the key is not equal to the value.
+
+    Args:
+    -----
+
+        `dictionary` (`dict`): The dictionary to search for non-matching values.
+
+    Returns:
+    --------
+
+        `dict`: Dictionary containing key-value pairs where the key is not equal to the value.
+
+    """
+    non_matching_values = {}
+
+    for key, value in dictionary.items():
+        if key != value:
+            non_matching_values[key] = value
+
+    return non_matching_values
+
+
+def print_table(data):
+    """
+    Print a dictionary in tabular form.
+
+    Args:
+    -----
+
+        `data` (`dict`): The dictionary to be printed as a table.
+
+    """
+    # Convert dictionary to list of lists
+    table = [[key, value] for key, value in data.items()]
+
+    # Print table
+    print(tabulate(table, headers=["Key", "Value"]))
+
+
+def get_key_by_value(dictionary, value):
+    """
+    Get the key associated with a specific value in a dictionary.
+
+    Args:
+    -----
+
+        `dictionary` (`dict`): The dictionary to search for the value.
+        `value`: The value to search for in the dictionary.
+
+    Returns:
+    --------
+
+        Key associated with the specified value, or None if the value is not found in the dictionary.
+
+    """
+    for key, val in dictionary.items():
+        if val == value:
+            return key
+    return None  # Value not found
+
+
+def create_merged_dict(dict1, dict2):
+    """
+    Create a new dictionary by merging values from two dictionaries.
+
+    The function takes two dictionaries as input and returns a new dictionary
+    where the keys are the values from the first dictionary and the values are
+    the corresponding values from the second dictionary.
+
+    Args:
+    -----
+
+        `dict1` (`dict`): The first dictionary.
+        `dict2` (`dict`): The second dictionary.
+
+    Returns:
+    --------
+
+        `dict`: A new dictionary with merged values.
+
+    Examples:
+    ---------
+
+        >>> dict1 = {'apple': 'fruit', 'carrot': 'vegetable', 'banana': 'fruit'}
+        >>> dict2 = {'fruit': 'red', 'vegetable': 'orange'}
+        >>> merged_dict = create_merged_dict(dict1, dict2)
+        >>> merged_dict
+        {'fruit': 'red', 'vegetable': 'orange'}
+
+    """
+    merged_dict = {}
+    for key, value in dict1.items():
+        merged_dict[value] = dict2[key]
+    return merged_dict
 
 
 def create_dictionary(m):
@@ -633,52 +774,3 @@ if __name__ == "__main__":
     expression_overall[Symbol("p5") + Symbol("q5")] = 1
     expression_overall[Symbol("p5") * Symbol("q5")] = 0
     print(expression_overall)
-
-
-def table_form(data):
-    # Convert dictionary to list of lists
-    table = [[key, value] for key, value in data.items()]
-
-    # Print table
-    print(tabulate(table, headers=["Key", "Value"]))
-
-
-def get_key_by_value(dictionary, value):
-    for key, val in dictionary.items():
-        if val == value:
-            return key
-    return None  # Value not found
-
-
-def create_merged_dict(dict1, dict2):
-    """
-    Create a new dictionary by merging values from two dictionaries.
-
-    The function takes two dictionaries as input and returns a new dictionary
-    where the keys are the values from the first dictionary and the values are
-    the corresponding values from the second dictionary.
-
-    Args:
-    -----
-
-        `dict1` (`dict`): The first dictionary.
-        `dict2` (`dict`): The second dictionary.
-
-    Returns:
-    --------
-
-        `dict`: A new dictionary with merged values.
-
-    Examples:
-    ---------
-
-        >>> dict1 = {'apple': 'fruit', 'carrot': 'vegetable', 'banana': 'fruit'}
-        >>> dict2 = {'fruit': 'red', 'vegetable': 'orange'}
-        >>> merged_dict = create_merged_dictionary(dict1, dict2)
-        >>> merged_dict
-        {'fruit': 'red', 'vegetable': 'orange'}
-    """
-    merged_dict = {}
-    for key, value in dict1.items():
-        merged_dict[value] = dict2[key]
-    return merged_dict
