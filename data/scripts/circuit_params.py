42b2c94eee031332bbe1c701645697b45baaa38c
Mostafa Atallah <m.ataallah.2014@gmail.com>
2024-03-10 15:43:29 -0400

    Added: a script for generating circuit paprmeters data

---
+++ data/scripts/circuit_params.py
@@ -0,0 +1,59 @@
+import sys
+import argparse
+import csv
+
+import pandas as pd
+from time import time
+from numpy import log2
+
+sys.path.append(f"./../../")
+
+from src import FactoringHamiltonian
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p", "--maxpow", type=int, default=40, required=False)
+    parser.add_argument("-n", "--number", type=int, default=200, required=False)
+    parser.add_argument("-r", "--replace", action="store_true")
+    parser.add_argument("-d", "--dir", type=str, default="./data/biprimes")
+    args = parser.parse_args()
+
+    number_of_biprimes = args.number
+    max_pow = args.maxpow
+
+    biprimes = []
+    file_in = f"./../biprimes/biprimes_maxpow_{max_pow}_number_{number_of_biprimes}.csv"
+
+    df = pd.read_csv(file_in)
+    biprimes = list(df["m=p*q"].values)
+
+    file_out = (
+        f"./../results/circuit_params_maxpow_{max_pow}_number_{number_of_biprimes}.csv"
+    )
+
+    with open(file_out, "w", newline="") as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(
+            [
+                "Biprime",
+                "Number of Qubits",
+                "Number of CNOTs",
+                "Log2(Biprime)",
+                "Log2(Number of Qubits)",
+                "Log2(Number of CNOTs)",
+            ]
+        )  # Header row
+
+        t = time()
+        for m in biprimes:
+            print(f"Considering biprime: {m}")
+            H = FactoringHamiltonian(m).hamiltonian
+            n_qubits = len(H.bits)
+            n_cnots = H.n_cnots
+            result = [m, n_qubits, n_cnots, log2(m), log2(n_qubits), log2(n_cnots)]
+            print("Results:")
+            print(result)
+            writer.writerow(result)
+            print(f"Computation finished ({round((time()-t)/60, 3)} min)")
+
+    print(f"All computations finished. Results saved in {file_out}.")
