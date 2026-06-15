79b4cbacf91a62014ecdd034bcbb96ad52c5a07a
Mostafa Atallah <m.ataallah.2014@gmail.com>
2026-06-15 19:10:15 -0400

    Restructure: move outputs to results/, CLI to scripts/

--- scripts/primes_gen.py
+++ scripts/primes_gen.py
@@ -80,7 +80,7 @@ if __name__ == "__main__":
     parser.add_argument("-p", "--maxpow", type=int, default=40, required=False)
     parser.add_argument("-n", "--number", type=int, default=200, required=False)
     parser.add_argument("-r", "--replace", action="store_true")
-    parser.add_argument("-d", "--dir", type=str, default="./data/biprimes")
+    parser.add_argument("-d", "--dir", type=str, default="./results/biprimes")
     args = parser.parse_args()
 
     # threshold = sys.maxsize
